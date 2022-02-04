c6075f81843f7366a56dbf2b03be74750d20074c
jeevan <jeevanmalavi07@gmail.com>
2022-02-04 17:24:58 +0530

    master

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'directives';
+  iscondition= true;
+  monthno="";
+  no = 1;
+
+  today = new Date()
+
+  months=["jan", "feb", "mar", "apr", "may", "jun", "jull", "aug", "oct", "sep", "nov", "dec"]
+
+ngOnInit(): void{
+
+  setInterval(()=>{
+    this.iscondition = !this.iscondition;
+  },1000);
+}
+
+changeme(event:Event){
+  let ctrl= <HTMLSelectElement>event.target;
+  let mon =  Number(ctrl.value);
+  this.monthno=this.months[mon -1];
+}
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SqrtPipe } from './app.sqrt';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SqrtPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
